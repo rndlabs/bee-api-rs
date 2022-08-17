7472cfd6c8ea9d0d22330b445f39cc978b72ffac
mfw78 <mfw78@protonmail.com>
2022-08-17 14:17:51 +0000

    feat: bytes post api

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,55 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
+
+pub struct SwarmUploadConfig {
+    pub stamp: String,
+    pub pin: Option<bool>,
+    // pub encrypt: Option<String>,
+    pub tag: Option<String>,
+    pub deferred: Option<bool>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SwarmReference {
+    pub ref_: String,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+
+// upload the data to the swarm using the bytes endpoint
+// should return the reference from swarm
+pub async fn bytes_post(client: Client, base_uri: String, data: Vec<u8>, config: SwarmUploadConfig) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let mut headers = reqwest::header::HeaderMap::new();
+
+    // process the config
+    headers.insert("swarm-postage-batch-id", config.stamp.parse().unwrap());
+    if let Some(pin) = config.pin && pin {
+        headers.insert("swarm-pin", "true".parse().unwrap());
     }
+    if let Some(tag) = config.tag {
+        headers.insert("swarm-tag", tag.parse().unwrap());
+    }
+    if let Some(deferred) = config.deferred && !deferred {
+        headers.insert("swarm-deferred", "false".parse().unwrap());
+    }
+
+    let res = client
+        .post(format!("{}/bytes", base_uri))
+        .body(data)
+        .headers(headers)
+        .send()
+        .await?;
+    let res = res.json::<SwarmReference>().await?;
+    Ok(res.ref_.as_bytes().to_vec())
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
