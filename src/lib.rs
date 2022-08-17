30038dd12bc15ff345d9cbfd24066de0b2882b60
mfw78 <mfw78@protonmail.com>
2022-08-17 18:01:16 +0000

    feat: bytes get

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+use std::error::Error;
+
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
+pub struct BeeConfig {
+    pub upload: Option<UploadConfig>,
+}
 
-pub struct SwarmUploadConfig {
+pub struct UploadConfig {
     pub stamp: String,
     pub pin: Option<bool>,
     // pub encrypt: Option<String>,
@@ -15,10 +20,40 @@ pub struct SwarmReference {
     pub ref_: String,
 }
 
+// download the data from the swarm using the bytes endpoint
+pub async fn bytes_get(
+    client: Client,
+    base_uri: String,
+    ref_: String,
+) -> Result<(Vec<u8>, String), Box<dyn Error>> {
+    let url = format!("{}/bytes/{}", base_uri, ref_);
+    let res = client.get(&url).send().await?;
+
+    // bubble up if there is an error
+    if !res.status().is_success() {
+        return Err(Box::new(res.error_for_status().unwrap_err()));
+    }
+
+    // read the response body
+    let content_type = if let Some(content_type) = res.headers().get("Content-Type") {
+        content_type.to_str()?.to_string()
+    } else {
+        "application/octet-stream".to_string()
+    };
+
+    let data = &res.bytes().await?;
+
+    Ok((data.to_vec(), content_type))
+}
 
 // upload the data to the swarm using the bytes endpoint
 // should return the reference from swarm
-pub async fn bytes_post(client: Client, base_uri: String, data: Vec<u8>, config: SwarmUploadConfig) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub async fn bytes_post(
+    client: Client,
+    base_uri: String,
+    data: Vec<u8>,
+    config: &UploadConfig,
+) -> Result<SwarmReference, Box<dyn Error>> {
     let mut headers = reqwest::header::HeaderMap::new();
 
     // process the config
@@ -26,10 +61,10 @@ pub async fn bytes_post(client: Client, base_uri: String, data: Vec<u8>, config:
     if let Some(pin) = config.pin && pin {
         headers.insert("swarm-pin", "true".parse().unwrap());
     }
-    if let Some(tag) = config.tag {
+    if let Some(tag) = &config.tag {
         headers.insert("swarm-tag", tag.parse().unwrap());
     }
-    if let Some(deferred) = config.deferred && !deferred {
+    if let Some(deferred) = &config.deferred && !deferred {
         headers.insert("swarm-deferred", "false".parse().unwrap());
     }
 
@@ -40,7 +75,7 @@ pub async fn bytes_post(client: Client, base_uri: String, data: Vec<u8>, config:
         .send()
         .await?;
     let res = res.json::<SwarmReference>().await?;
-    Ok(res.ref_.as_bytes().to_vec())
+    Ok(res)
 }
 
 // #[cfg(test)]
